9c587512354e87869d9c0d11fa53cf28179a32cc
Marc Vousden <marcamillian@gmail.com>
2024-01-30 10:48:43 -0500

    Feature: Adding router middleware
    The [tutorial here])https://gowebexamples.com/routes-using-gorilla-mux/) is using `gorilla/mux` - when I added this and tired to run I got an error that indicated malicious code
    
    It seems that the `gorilla/mux` project was deprecated - and then subsiquently brought back.
    
    As I was getting a runtime error from my anti-virus - I used `go-chi/chi` instead

--- main.go
+++ main.go
@@ -3,16 +3,39 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to my website!!!!")
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hi"))
 	})
 
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
 
-	http.ListenAndServe(":80", nil)
+		title := chi.URLParam(r, "title")
+		page := chi.URLParam(r, "page")
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+
+	})
+
+	http.ListenAndServe(":3333", r)
 }
