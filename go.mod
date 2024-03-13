4d73ed5d1b841cf75e02922db76ee131ed1a8433
Marc Vousden <marcamillian@gmail.com>
2024-03-13 12:39:03 -0400

    test: Adds tests for the html template endpoint
    - Checks for the status
    - Checks for elements in the response

--- go.mod
+++ go.mod
@@ -3,12 +3,16 @@ module example.com/skull
 go 1.21.6
 
 require (
+	github.com/andybalholm/cascadia v1.3.2 // indirect
 	github.com/dolthub/maphash v0.1.0 // indirect
 	github.com/go-chi/chi/v5 v5.0.11 // indirect
+	github.com/go-shiori/dom v0.0.0-20230515143342-73569d674e1c // indirect
+	github.com/gogs/chardet v0.0.0-20211120154057-b7413eaefb8f // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/klauspost/compress v1.17.5 // indirect
 	github.com/lxzan/gws v1.8.1 // indirect
 	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
 
 replace handlers => /handlers
--- handlers/htmlTemplate_test.go
+++ handlers/htmlTemplate_test.go
@@ -4,14 +4,22 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/go-shiori/dom"
+	"golang.org/x/net/html"
 )
 
 func TestHandleTodoList(t *testing.T) {
 
 	// expected values
 	expectedStatus := http.StatusOK
+	expectedElements := []string{
+		"h1",
+		"ul",
+		"li",
+	}
 
-	// make the request
+	// === Make the request ====
 	req, err := http.NewRequest("GET", "/todo/MarcList", nil)
 	if err != nil {
 		t.Fatal(err)
@@ -22,10 +30,25 @@ func TestHandleTodoList(t *testing.T) {
 	handler := http.HandlerFunc(HandleTodoList)
 	handler.ServeHTTP(rr, req)
 
+	// === Check the response ===
+
 	// check the status
 	if status := rr.Code; status != expectedStatus {
 		t.Errorf("handler returned the wrong status code: got %v wanted %v",
 			status, expectedStatus)
+	}
+
+	// querying the DOM - https://ahmadrosid.com/blog/how-to-query-html-dom-in-golang
+	document, err := html.Parse(rr.Body)
+
+	if err != nil {
+		t.Errorf("coudln't parse document body")
+	}
+
+	for _, selector := range expectedElements {
+		if dom.QuerySelector(document, selector) == nil {
+			t.Errorf("document doesn't contain element with selector %s", selector)
+		}
 	}
 
 }
